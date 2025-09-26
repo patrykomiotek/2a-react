e1667384b27af3867ad93759a69efb5655f31e6e
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-09-26 15:45:16 +0200

    feat: with use state

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import "./App.css";
+import { useState } from "react";
 import { Button } from "./ui/Button";
 import { Text } from "./ui/Text";
 
 function App() {
-  let count = 0;
+  // let count = 0;
+  const state = useState(0); // 0 - initial value
+  // state[0] - value
+  // state[1] - callback
 
   const handleClickOne = () => {
     // alert("Hello world!");
-    count = count + 1;
-    console.log({ count });
+    // count = count + 1;
+    state[1]((currentValue) => currentValue + 1);
+    console.log({ count: state[0] });
   };
 
   const handleClickTwo = () => {
     // alert("Hello Techni!");
-    count = count + 1;
-    console.log({ count });
+    // count = count + 1;
+    state[1]((currentValue) => currentValue + 1);
+    console.log({ count: state[0] });
   };
 
   return (
     <div>
-      <Text>Count: {count}</Text>
+      <Text>Count: {state[0]}</Text>
       <Button
         label="Click"
         onClick={handleClickOne}
