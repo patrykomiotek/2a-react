3bf29d9119c469be24b5f1a726bcfabcf65bc690
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-10 16:01:03 +0200

    feat: fetching data using rest api

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,33 +1,41 @@
-const data = [
-  {
-    id: 1,
-    name: "Rick Sanchez",
-    status: "Alive",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-  },
-  {
-    id: 2,
-    name: "Rick Sanchez",
-    status: "Alive",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-  },
-];
+import { useEffect, useState } from "react";
+
+interface CharacterDto {
+  id: number;
+  image: string;
+  name: string;
+  status: "Alive" | "Dead";
+  species: string;
+}
 
 export const Characters = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<CharacterDto[]>([]);
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then((data) => {
+        setIsLoading(false);
+        setData(data.results);
+      });
+  }, []);
+
   return (
-    <div className="space-y-4">
-      {data.map((elem) => (
-        <div key={elem.id} className="flex gap-2">
-          <img src={elem.image} className="rounded-full h-24" />
-          <div>
-            <h2 className="text-2xl">{elem.name}</h2>
-            <p className="text-slate-500">{elem.status}</p>
-            <p className="text-slate-500">{elem.species}</p>
+    <div>
+      {isLoading && <p>Loading...</p>}
+      <div className="space-y-4">
+        {data.map((elem) => (
+          <div key={elem.id} className="flex gap-2">
+            <img src={elem.image} className="rounded-full h-24" />
+            <div>
+              <h2 className="text-2xl">{elem.name}</h2>
+              <p className="text-slate-500">{elem.status}</p>
+              <p className="text-slate-500">{elem.species}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
