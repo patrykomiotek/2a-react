056b632b129d189ff35f354bff56083eaef747c8
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-17 15:45:40 +0200

    refactor: app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,13 @@
-import "./App.css";
-import { useState } from "react";
-import { v4 as uuiv4 } from "uuid";
-// import { Button } from "./ui/Button";
-// import { Text } from "./ui/Text";
-// import { MagicButton } from "./ui/MagicButton";
-import { RegistrationForm } from "./components/RegistrationForm";
-import { RegistrationFormRefs } from "./components/RegistrationFormRefs";
-import { Viewport } from "./components/Viewport";
-import { Counter } from "./components/Counter";
-import { Characters } from "./components/Characters";
-import { Character } from "./components/Character";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
+import './App.css';
+
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './routes';
 
 function App() {
-  // let count = 0;
-  const [uuid, setUuid] = useState(uuiv4());
-  const [count, setCount] = useState(0); // 0 - initial value
-  // const state = useState(0); // 0 - initial value
-  // state[0] - value
-  // state[1] - callback
-
-  const handleClickOne = () => {
-    // alert("Hello world!");
-    // count = count + 1;
-    setCount((currentValue) => currentValue + 1);
-    console.log({ count });
-    setUuid(uuiv4());
-  };
-
-  const handleClickTwo = () => {
-    // alert("Hello Techni!");
-    // count = count + 1;
-    setCount((currentValue) => currentValue + 1);
-    console.log({ count });
-  };
-
   return (
     <div className="container">
       <RouterProvider router={router} />
-      {/* <Character /> */}
-      {/* <Characters /> */}
-      {/* <Counter /> */}
-      {/* <Viewport /> */}
-      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
-      {/* <RegistrationForm /> */}
-      {/* <RegistrationFormRefs /> */}
-      {/* <Text>{uuid}</Text>
-      <Text>Count: {count}</Text>
-      <div>
-        <Button label="Click" onClick={handleClickOne} />
-        <Button label="Click me again" onClick={handleClickTwo} />
-      </div>
-      <div>
-        <MagicButton label="Magic Button" onClick={handleClickTwo} />
-        <MagicButton label="Magic Button" onClick={handleClickTwo} />
-      </div> */}
     </div>
   );
 }
