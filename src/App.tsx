0ec811f29a67312506c0042857c2a1d9dc2460c7
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-17 15:50:09 +0200

    feat: console log

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 
 function App() {
+  console.log('test 1234');
+
   return (
     <div className="container">
       <RouterProvider router={router} />
