6b57ed23133f8e34125c93954a9b8eb9931791c1
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-24 11:54:13 +0200

    feat: react hook form intro

---
+++ src/components/CreateProductForm.tsx
@@ -0,0 +1,66 @@
+import {
+  useRef,
+  useState,
+  type ChangeEventHandler,
+  type FormEventHandler,
+  // createRef,
+} from 'react';
+import { useForm } from 'react-hook-form';
+
+import { Button } from '../ui/Button';
+import { Input } from '../ui/Input';
+
+export const CreateProductForm = () => {
+  const { register, handleSubmit } = useForm();
+  // const [passwordError, setPasswordError] = useState(false);
+  const emailFieldRef = useRef<HTMLInputElement>(null);
+  const passwordFieldRef = useRef<HTMLInputElement>(null);
+  const passwordErrorFieldRef = useRef<HTMLInputElement>(null);
+  const languageFieldRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit: FormEventHandler = (event) => {
+    event.preventDefault();
+    const emailValue = emailFieldRef.current?.value || '';
+    const passwordValue = passwordFieldRef.current?.value || '';
+
+    if (passwordValue.length <= 3) {
+      // setPasswordError(true);
+      const passwordError = passwordErrorFieldRef.current;
+      if (passwordError) {
+        passwordError.innerText = 'Too short';
+        // passwordError.style.color = "red";
+      }
+    }
+
+    const languageValue = languageFieldRef.current?.value || '';
+    // FormData
+    // state
+    console.log({
+      email: emailValue,
+      password: passwordValue,
+      language: languageValue,
+    });
+  };
+
+  const handleLanguageValidation: ChangeEventHandler<HTMLInputElement> = () => {
+    const languageField = languageFieldRef.current;
+    const languageValue = languageField?.value;
+    if (languageValue && languageValue === 'php') {
+      languageField.value = '***';
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="space-y-2">
+        <Input ref={emailFieldRef} label="E-mail" type="email" />
+
+        <Input ref={passwordFieldRef} label="Password" type="password" />
+        <p className="text-red-500" ref={passwordErrorFieldRef}></p>
+
+        <Input ref={languageFieldRef} label="Language" />
+      </div>
+      <Button type="submit" label="Send" />
+    </form>
+  );
+};
