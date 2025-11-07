215547fd04dbf3f0d45da85f7b982d317e722862
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-11-07 15:07:34 +0100

    feat: with session storage

--- src/features/hotels/components/HotelDetails.tsx
+++ src/features/hotels/components/HotelDetails.tsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
 import type { HotelDto } from '../contracts/hotel.dto';
+import { Button } from '../../../ui/Button';
 
 type Props = {
   hotel: HotelDto;
 };
 
 export const HotelDetails = ({ hotel }: Props) => {
+  const handleReserve = (hotel: HotelDto) => {
+    //
+    sessionStorage.setItem('hotel', JSON.stringify(hotel));
+    // sessionStorage.getItem('hotel'); // next screen
+  };
+
   return (
     <div className="space-y-2">
       <div>
@@ -16,6 +23,9 @@ export const HotelDetails = ({ hotel }: Props) => {
         <Link to="/hotels" className="text-blue-500">
           Back to hotels
         </Link>
+      </div>
+      <div>
+        <Button label="Reserve" onClick={() => handleReserve(hotel)} />
       </div>
     </div>
   );
